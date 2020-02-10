b4b3334d764833ad79fd3572bdc31bcbfb48d2ec
dhall88 <dhall87@gmail.com>
2020-02-10 10:03:02 -0500

    added from to select math type and difficulty, these will call components

--- src/sub-apps/math-drills/MathDrills.js
+++ src/sub-apps/math-drills/MathDrills.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
 
 export default class MathDrills extends Component {
+  state = {
+    difficulty:'',
+    type:''
+  }
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
   render() {
     return(
-      <h1>TEST</h1>
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Difficulty
+          <select onChange={this.handleChange} id = 'difficulty'>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </label>
+        <label>
+          Type
+          <select onChange={this.handleChange} id = 'type'>
+            <option value="Numbers">Numbers</option>
+            <option value="Addtion">Arithemtic</option>
+            <option value="Subtration">Subtration</option>
+            <option value="Multiplication">Multiplication</option>
+            <option value="Division">Division</option>
+            <option value="OrderOfOperations">Order Of Operations</option>
+          </select>
+        </label>
+        <input type="submit" />
+        </form>
+
+      </div>
     )
   }
 }
