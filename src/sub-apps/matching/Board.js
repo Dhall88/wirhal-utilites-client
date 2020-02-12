dc41d08c045f9ff899289fd4e09462e9d9259c43
dhall88 <dhall87@gmail.com>
2020-02-11 20:47:49 -0500

    matching game logic working

--- src/sub-apps/matching/Board.js
+++ src/sub-apps/matching/Board.js
@@ -13,23 +13,40 @@ export default class Board extends Component {
     refIndex:[],
     numSquares:0,
     width:300,
-    difficulty:"8",
-    scale: 3
+    difficulty:"0",
+    scale: 3,
+    symbols:[]
   }
 
   childRef0=React.createRef();childRef1=React.createRef();childRef2=React.createRef(); childRef3=React.createRef();
   childRef4=React.createRef(); childRef5=React.createRef();childRef6=React.createRef(); childRef7=React.createRef();
   childRef8=React.createRef(); childRef9=React.createRef();childRef10=React.createRef(); childRef11=React.createRef();
   childRef12=React.createRef(); childRef13=React.createRef();childRef14=React.createRef(); childRef15=React.createRef();
-  symbolArr=['x','y','p','o','a','b','g','w']
+  symbolArr=['c','c','p','p','a','a','g','g','x','x','o','o','b','b','w','w']
+  difficulty="0"
+  squares=[]
 
   handleChange = (event) => {
     this.setState({ [event.target.id]: event.target.value })
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.difficulty=this.state.difficulty
+    let symbols = this.symbolArr.slice(this.difficulty)
+    symbols = symbols.sort(function() {
+      return .5 -Math.random()
+    })
+    console.log(symbols);
+    console.log(symbols);
+    this.setState({
+      symbols: symbols
+    })
+  }
+
 
   callback = (childSymbol, childId) => {
-
+    console.log(childSymbol);
       this.setState({
         stored: [...this.state.stored, childSymbol],
         refIndex: [...this.state.refIndex, childId]
@@ -38,7 +55,6 @@ export default class Board extends Component {
       setTimeout(()=>{
         let stored = this.state.stored
         let refs = [...this.state.refIndex]
-        console.log(refs);
         if (stored.length===2){
           if (stored[0]===stored[1]) {
             this.setState({
@@ -66,24 +82,30 @@ export default class Board extends Component {
 // }
   render() {
     let squares=[];
-    for(let i = 0;i<this.state.difficulty;i++) {
+    // let symbols = this.symbolArr.slice(8)
+    // symbols = symbols.sort(function() {
+    //   return .5 -Math.random()
+    // })
+    // console.log(symbols);
+    for(let i = 0;i<this.difficulty;i++) {
       squares.push(
-          <Square ref={this[`childRef${i}`]} id={i} symbol={this.symbolArr[Math.floor(i/2)]} parentCallback={this.callback}/>
+          <Square ref={this[`childRef${i}`]} id={i} symbol={this.state.symbols[i]} parentCallback={this.callback}/>
       )
     }
 
-    squares.sort(function (a, b) { return 0.5 - Math.random() })
+
     return(
       <>
       <form onSubmit={this.handleSubmit}>
         <label>
           Difficulty
           <select onChange={this.handleChange} id = 'difficulty'>
+            <option>Select</option>
             <option value={8}>Easy</option>
             <option value={12}>Medium</option>
             <option value={16}>Hard</option>
           </select>
-          <input type='submit'/>
+          <input type='submit' />
         </label>
         </form>
         {this.state.matchBoolean===true?
