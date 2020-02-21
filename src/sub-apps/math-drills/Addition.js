7208acaacb828ee145ba00a62c25f95e8897cc4f
dhall88 <dhall87@gmail.com>
2020-02-21 09:57:22 -0500

    correctly grading problems

--- src/sub-apps/math-drills/Addition.js
+++ src/sub-apps/math-drills/Addition.js
@@ -3,17 +3,54 @@ import React, { Component } from 'react'
 export default class Addition extends Component {
   state = {
     numArr:[],
-    score:0
+    score:0,
+    answer:"",
+    dispHorray: false,
+    dispTryAgain: false
   }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value ,showComponent:true})
+  }
+
   questionArr = () => {
     let arr = []
     for(let i = 1; i<=this.props.difficulty; i++) {
       arr.push(Math.round(Math.random()*Math.pow(this.props.difficulty,3)))
     }
     this.setState({
-      numArr: arr
+      numArr: arr,
+      answer: ""
     })
     console.log(arr);
+  }
+
+  check = () => {
+    let answer = 0;
+    for(let i = 0; i<this.state.numArr.length; i++) {
+      answer+=this.state.numArr[i]
+    }
+    console.log(answer);
+    if(parseInt(this.state.answer)===answer) {
+      this.setState({
+        dispHorray: true,
+        answer: ""
+      })
+      setTimeout(()=> {
+        this.setState({
+          dispHorray: false
+        })
+        this.questionArr()},3000)
+    } else {
+      this.setState({
+        dispTryAgain: true
+      })
+      setTimeout(()=> {
+        this.setState({
+          dispTryAgain: false
+        })
+      },3000)
+    }
   }
 
   render() {
@@ -26,6 +63,12 @@ export default class Addition extends Component {
             return <li>{num}</li>
           })}
         </ul>
+        <form onSubmit={this.check}>
+          <input onChange={this.handleChange} type="text" id="answer" />
+          <input type="submit" />
+        </form>
+        {this.state.dispHorray?<div>YAY</div>:''}
+        {this.state.dispTryAgain?<div>Try again</div>:'z'}
       </>
     )
   }
