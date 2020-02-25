34f731da5a994d4877e5f82060f43a49f99d3fd5
dhall88 <dhall87@gmail.com>
2020-02-25 10:24:30 -0500

    expanding bar trivia component, all but reset done

--- src/sub-apps/bar-trivia/BarTrivia.js
+++ src/sub-apps/bar-trivia/BarTrivia.js
@@ -1,64 +1,174 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import List from './List'
 // https://opentdb.com/api_token.php?command=request
 
 export default class BarTrivia extends Component {
   state = {
     token:'',
     questions:[],
-    questionCounter: 0,
     displayQuestion: false,
     activeQuestion: {},
-    score:0
+    score:0,
+    displayResult:false,
+    length:10,
+    displayCounter: false
   }
+  questionCounter=0
 
+//   this.setState({
+//     token:resp.data.token
+//   })
+// });
   componentDidMount = () => {
     axios.get('https://opentdb.com/api_token.php?command=request').then(resp => {
       this.setState({
-        token:resp.data.token
+        token: resp.data.token
       })
-    });
-  }
+    })
+}
+
+handleChange = (event) => {
+  this.setState({ [event.target.id]: event.target.value ,showComponent:true})
+}
 
   getQuestions = () => {
-    axios.get(`https://opentdb.com/api.php?amount=10&token=${this.state.token}`)
-    .then(resp => {
-      this.setState({
-        questions: resp.data.results
-      })
+  axios.get(`https://opentdb.com/api.php?amount=15&token=${this.state.token}`)
+  .then(resp => {
+    console.log(resp);
+    this.setState({
+      questions: resp.data.results,
+      activeQuestion: resp.data.results[0],
+      displayQuestion: true,
+      displayCounter: true
     })
-  }
+  })
+
+}
 
   nextQuestion = (event) => {
-    event.preventDefault();
+    // console.log(this.state.activeQuestion);
+    // event.preventDefault();
+    this.questionCounter++;
     this.setState({
-      activeQuestion: this.state.questions[this.state.questionCounter],
-      questionCounter: (this.state.questionCounter+1),
+      activeQuestion: this.state.questions[this.questionCounter],
       displayQuestion: true
     })
-    console.log(this.state.questions);
   }
 
-  generateList = () => {
-
+  correct = (event) => {
+    console.log('in correct');
+    event.preventDefault();
+    this.setState({
+      score: this.state.score+10,
+      displayQuestion: false,
+      answer: false,
+      displayResult: true
+    })
+    this.nextQuestion()
+    setTimeout(() => {
+      this.setState({
+        displayQuestion: true,
+        displayResult: false
+      })
+    },1000)
   }
+
+  incorrect = (event) => {
+    event.preventDefault();
+    console.log('in incorrect');
+    this.setState({
+      displayQuestion: false,
+      answer: false,
+      displayResult: true
+    })
+    setTimeout(() => {
+      this.nextQuestion()
+      this.setState({
+        displayQuestion: true,
+        displayResult: false
+      })
+    },1000)
+  }
+
+  replaceSymbols = (str) => {
+    return str.replace(/&quot;/g,`"`).replace(/&#039;/g,"'").replace(/&amp;/g,"&")
+  }
+
 
   render() {
-    this.generateList()
+    let listArr=[];
+    if (this.state.activeQuestion.type==="multiple") {
+      let randPosition = Math.floor(Math.random()*4)
+      if(randPosition===3){
+        for(let i = 0; i<3;i++) {
+          listArr.push(<li onClick={this.incorrect}>{this.replaceSymbols(this.state.activeQuestion.incorrect_answers[i])}</li>)
+          if (i===2) {
+            listArr.push(<li onClick={this.correct}>{this.replaceSymbols(this.state.activeQuestion.correct_answer)}</li>)
+          }
+        }
+      }
+
+      else {
+        for(let i = 0; i<3;i++) {
+          if (i===randPosition) {
+            listArr.push(<li onClick={this.correct}>{this.replaceSymbols(this.state.activeQuestion.correct_answer)}</li>,<li onClick={this.incorrect}>{this.replaceSymbols(this.state.activeQuestion.incorrect_answers[i])}</li>)
+          } else {
+            listArr.push(<li onClick={this.incorrect}>{this.replaceSymbols(this.state.activeQuestion.incorrect_answers[i])}</li>)
+          }
+        }
+      }
+    }
+
+    else {
+      if (this.state.activeQuestion.correct_answer==='True') {
+        listArr.push(<li onClick={this.correct}>True</li>, <li onClick={this.incorrect}>False</li>)
+      } else {
+        listArr.push(<li onClick={this.incorrect}>True</li>, <li onClick={this.correct}>False</li>)
+      }
+    }
+
 
     return(
       <div>
-        <h1 onClick={this.getQuestions}>Bar Triva</h1>
-        <div onClick={this.nextQuestion}>Get question</div>
+        <h1>Bar Triva</h1>
+        <form onSubmit={this.getQuestions}>
+          <label>
+            Difficulty
+            <select onChange={this.handleChange} id = 'length'>
+              <option value={10}>Short</option>
+              <option value={20}>Medium</option>
+              <option value={30}>Long</option>
+            </select>
+          </label>
+          <input type="submit" />
+        </form>
+        {this.state.displayCounter?
+          <>
+        <div>{this.state.score}</div>
+        <div>{this.questionCounter+1}/{this.state.length}</div>
+          </>
+        :''}
         {this.state.displayQuestion?
           <>
-        <p>{this.state.activeQuestion.question.replace(/&quot;/g,`"`).replace(/&#039;/g,"'")}</p>
-        <List questionData={this.state.activeQuestion} />
-        </>
+            <p>{this.state.activeQuestion.question.replace(/&quot;/g,`"`).replace(/&#039;/g,"'").replace(/&amp;/g,"'")}</p>
+            <ul>{listArr}</ul>
+          </>
         :''
       }
+
+      {this.state.displayResult?
+      <>
+        {this.state.answer?
+          <div>
+            <p>Congratulations! 10 points to Gryphindor</p>
+          </div>
+          :
+          <div>
+            <p>Sorry, the correct answer was {this.state.activeQuestion.correct_answer}</p>
+          </div>}
+      </> : ''}
+
       </div>
     )
   }
--- src/sub-apps/bar-trivia/List.js
+++ src/sub-apps/bar-trivia/List.js
@@ -4,37 +4,7 @@ export default class List extends Component {
 
 
   render() {
-    let listArr=[];
-    console.log(this.props.questionData.type);
-    if (this.props.questionData.type==="multiple") {
-      let randPosition = Math.floor(Math.random()*4)
-      if(randPosition===3){
-        for(let i = 0; i<3;i++) {
-          listArr.push(<li>{this.props.questionData.incorrect_answers[i]}</li>)
-          if (i===2) {
-            listArr.push(<li>{this.props.questionData.correct_answer}</li>)
-          }
-        }
-      }
 
-      else {
-        for(let i = 0; i<3;i++) {
-          if (i===randPosition) {
-            listArr.push(<li>{this.props.questionData.correct_answer}</li>,<li>{this.props.questionData.incorrect_answers[i]}</li>)
-          } else {
-            listArr.push(<li>{this.props.questionData.incorrect_answers[i]}</li>)
-          }
-        }
-      }
-    }
-
-    else {
-      if (this.props.questionData.correct_answer==='True') {
-        listArr.push(<li>True</li>, <li>False</li>)
-      } else {
-        listArr.push(<li>True</li>, <li>False</li>)
-      }
-    }
 
     return (
       <div>{listArr}</div>
