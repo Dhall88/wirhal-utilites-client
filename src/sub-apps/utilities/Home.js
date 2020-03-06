4a91d409654d27869fc64b1a28d5f0dc870fd5dc
David Hall <davidhall@Davids-MacBook-Pro.local>
2020-03-06 08:42:20 -0500

    sound glitch with googledev tools

--- src/sub-apps/utilities/Home.js
+++ src/sub-apps/utilities/Home.js
@@ -12,13 +12,30 @@ const FlexWrapper=styled.div`
   font-size: 30px;
 `;
 
+
 export default class Home extends Component {
+	state = {
+		playMusic: false
+	}
+	audioSrc=''
+	componentDidMount() {
+		this.aduioSrc='https://vgmdownloads.com/soundtracks/super-mario-yoshis-island-original-sound-version/lqomnkkv/03.%20Yoshi%20Island.mp3'
+	}
+	playMusic = (event) => {
+		event.preventDefault();
+		this.ListeningStateChangedEvent({
+			playMusic: true
+		})
+	}
 	render() {
 		return (
 			<FlexWrapper>
-      	<h1>Home</h1>
+      			<h1>Home</h1>
 				<h3>Inspired by my family, for my family, Wirhal Family Utilities is a collection of apps and games.</h3>
-
+				<button onClick={this.playMusic}>Play Music</button>
+				{playMusic?
+				<Audio source={this.audioSrc} />
+				:''}
 			</FlexWrapper>
 		);
 	}
