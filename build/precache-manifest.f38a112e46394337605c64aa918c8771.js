deae424ff032376ab0f1d25b4902f9b0ed0c09f4
David Hall <davidhall@Davids-MacBook-Pro.local>
2020-03-05 20:20:48 -0500

    heroku fix

---
+++ build/precache-manifest.f38a112e46394337605c64aa918c8771.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180696729a1500906bf98b1a40ece3c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5122f4ab76bc063b1e9",
+    "url": "/static/css/2.c0851dc3.chunk.css"
+  },
+  {
+    "revision": "1e363595bf935b6ad060",
+    "url": "/static/css/main.5a8c6179.chunk.css"
+  },
+  {
+    "revision": "c5122f4ab76bc063b1e9",
+    "url": "/static/js/2.72a3165a.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/static/js/2.72a3165a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e363595bf935b6ad060",
+    "url": "/static/js/main.20814d95.chunk.js"
+  },
+  {
+    "revision": "100b36adbcb3662e324d",
+    "url": "/static/js/runtime-main.d59f0445.js"
+  },
+  {
+    "revision": "2ad2496a2b5a33c1746542c77a0b4b7a",
+    "url": "/static/media/baseball-cap.2ad2496a.png"
+  },
+  {
+    "revision": "e87fecb04a0f4d14ab18cc8b4a1801d7",
+    "url": "/static/media/coat.e87fecb0.png"
+  },
+  {
+    "revision": "f029667a7df3854438022d30c2db299a",
+    "url": "/static/media/long-sleeve-shirt.f029667a.png"
+  },
+  {
+    "revision": "eda652514f9b4b094a1f2fed01264f7f",
+    "url": "/static/media/rain-boots.eda65251.png"
+  },
+  {
+    "revision": "62cda366f3c52641d7982f26796a3303",
+    "url": "/static/media/raincoat.62cda366.png"
+  },
+  {
+    "revision": "bb03960a1fb56cc5945fe7ff1ea94ca9",
+    "url": "/static/media/shorts.bb03960a.png"
+  },
+  {
+    "revision": "9b2cb18a092964c9f027133a6d618b00",
+    "url": "/static/media/sneakers.9b2cb18a.png"
+  },
+  {
+    "revision": "c02572075c0642ca2e38e68a37780125",
+    "url": "/static/media/sunglasses.c0257207.png"
+  },
+  {
+    "revision": "f6215725ee3158b6f9bd0025cca142d4",
+    "url": "/static/media/sweater.f6215725.png"
+  },
+  {
+    "revision": "03bb19fb6e1d7cd7ed5b4ce4ab4aed9e",
+    "url": "/static/media/t-shirt.03bb19fb.png"
+  },
+  {
+    "revision": "307839dbfbb7f3ffab05587f8bc6f0c0",
+    "url": "/static/media/umbrella.307839db.png"
+  },
+  {
+    "revision": "018beca482e9aa5fa057dce5b4bf62e8",
+    "url": "/static/media/windbreaker.018beca4.png"
+  },
+  {
+    "revision": "29ca6cd7c157d6d33f87ca1c7fe04b4e",
+    "url": "/static/media/winter-hat.29ca6cd7.png"
+  }
+]);
